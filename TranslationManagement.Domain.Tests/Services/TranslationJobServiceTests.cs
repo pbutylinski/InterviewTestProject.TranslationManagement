8cd6a4ba3eca3e79ead5ce375fa0965770666230
Lemur Solutions <pbutylinski@outlook.com>
2022-06-25 23:21:36 +0200

    Add unit tests to domain

---
+++ TranslationManagement.Domain.Tests/Services/TranslationJobServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Moq;
+using TranslationManagement.DataAccess;
+using TranslationManagement.DataAccess.Models;
+using TranslationManagement.Domain.Services;
+
+namespace TranslationManagement.Domain.Tests.Services
+{
+    public class TranslationJobServiceTests
+    {
+        private readonly TranslationJobService sut;
+        private readonly Fixture fixture;
+
+        private readonly Mock<IRepository<TranslationJob>> repositoryMock;
+        private readonly Mock<IPriceCalculationService> priceCalculationMock;
+        private readonly Mock<IUnreliableServiceWrapper> serviceWrapperMock;
+        private readonly Mock<IMapper> mapperMock;
+
+        public TranslationJobServiceTests()
+        {
+            this.repositoryMock = new Mock<IRepository<TranslationJob>>();
+            this.priceCalculationMock = new Mock<IPriceCalculationService>();
+            this.serviceWrapperMock = new Mock<IUnreliableServiceWrapper>();
+            this.mapperMock = new Mock<IMapper>();
+
+            this.sut = new TranslationJobService(
+                this.repositoryMock.Object,
+                this.priceCalculationMock.Object,
+                this.serviceWrapperMock.Object,
+                this.mapperMock.Object);
+
+            this.fixture = new Fixture();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.serviceWrapperMock.Reset();
+        }
+
+        [Test]
+        public async Task When_JobIsCreatedSuccessfully_Then_NotificationIsSent()
+        {
+            // Setup
+            var job1 = this.fixture.Create<Models.TranslationJob>();
+            var job2 = this.fixture.Create<TranslationJob>();
+            this.mapperMock.Setup(x => x.Map<TranslationJob>(It.IsAny<Models.TranslationJob>())).Returns(job2);
+            this.repositoryMock.Setup(x => x.Create(It.IsAny<TranslationJob>())).ReturnsAsync(1);
+
+            // Act
+            await this.sut.Create(job1);
+
+            // Assert
+            this.serviceWrapperMock.Verify(x => x.TrySendNewJobNotification(It.IsAny<int>()), Times.Once);
+        }
+
+        [Test]
+        public async Task When_JobIsNotCreatedSuccessfully_Then_NotificationWasNotSent()
+        {
+            // Setup
+            var job1 = this.fixture.Create<Models.TranslationJob>();
+            var job2 = this.fixture.Create<TranslationJob>();
+            this.mapperMock.Setup(x => x.Map<TranslationJob>(It.IsAny<Models.TranslationJob>())).Returns(job2);
+            this.repositoryMock.Setup(x => x.Create(It.IsAny<TranslationJob>())).ReturnsAsync(0);
+
+            // Act
+            await this.sut.Create(job1);
+
+            // Assert
+            this.serviceWrapperMock.Verify(x => x.TrySendNewJobNotification(It.IsAny<int>()), Times.Never);
+        }
+    }
+}
---
+++ TranslationManagement.Domain.Tests/Validators/JobStatusValidatorTests.cs
@@ -0,0 +1,70 @@
+﻿using TranslationManagement.Domain.Validators;
+using static TranslationManagement.Common.Constants.JobStatus;
+
+namespace TranslationManagement.Domain.Tests.Validators
+{
+    // Most tests here can be done using parametrized test cases
+    public class JobStatusValidatorTests
+    {
+        private readonly IJobStatusValidator sut;
+
+        public JobStatusValidatorTests()
+        {
+            this.sut = new JobStatusValidator();
+        }
+
+        [Test]
+        public void When_StatusMovesFromNewToInProgress_Then_ValidationPasses()
+        {
+            Assert.DoesNotThrow(() => this.sut.ValidateAndThrow(New, InProgress));
+        }
+
+        [Test]
+        public void When_StatusMovesInProgressToCompleted_Then_ValidationPasses()
+        {
+            Assert.DoesNotThrow(() => this.sut.ValidateAndThrow(InProgress, Completed));
+        }
+
+        [Test]
+        public void When_StatusesAreNull_Then_ValidationFails()
+        {
+            Assert.Throws(typeof(ValidationException), 
+                () => this.sut.ValidateAndThrow(null, null));
+        }
+
+        [Test]
+        public void When_StatusesAreEmpty_Then_ValidationFails()
+        {
+            Assert.Throws(typeof(ValidationException), 
+                () => this.sut.ValidateAndThrow(string.Empty, string.Empty));
+        }
+
+        [Test]
+        public void When_StatusesAreInvalid_Then_ValidationFails()
+        {
+            Assert.Throws(typeof(ValidationException),
+                () => this.sut.ValidateAndThrow("qwerty", "test123"));
+        }
+
+        [Test]
+        public void When_StatusMovesFromNewToCompleted_Then_ValidationFails()
+        {
+            Assert.Throws(typeof(ValidationException),
+                () => this.sut.ValidateAndThrow(New, Completed));
+        }
+
+        [Test]
+        public void When_StatusMovesFromCompletedToNew_Then_ValidationFails()
+        {
+            Assert.Throws(typeof(ValidationException),
+                () => this.sut.ValidateAndThrow(Completed, New));
+        }
+
+        [Test]
+        public void When_StatusMovesFromInProgressToNew_Then_ValidationFails()
+        {
+            Assert.Throws(typeof(ValidationException),
+                () => this.sut.ValidateAndThrow(InProgress, New));
+        }
+    }
+}
